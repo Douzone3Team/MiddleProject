d41690970d4064d3626579edc86f8aa31a576ecf
DESKTOP-7DI33P4\gktjd <039818>
2022-04-18 17:10:52 +0900

    hsr

--- argon-dashboard-react-master/server/server.js
+++ argon-dashboard-react-master/server/server.js
@@ -1,15 +1,52 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const PORT = 4000;
 const path = require('path');
+const cors = require('cors');
+const bodyParser = require("body-parser");
+// require('dotenv').config({path:path.join(__dirname, './db/db.env')});   //환경변수 세팅
 
 let socketList = {};
 //개발
 app.use(express.static(path.join(__dirname, '../client/public')));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
+
+app.use(express.static(path.join(__dirname, '../client/public')));
+app.use(bodyParser.urlencoded({ extended:false}))
+app.use(bodyParser.json());
+app.use(cors());
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
+
+app.post('/api/login',(req,res) => {
+    const data = req.body;   
+    const getId = data.id.id;
+    const getPass = data.pass.pass;
+    console.log(data);
+    console.log(getId + " " + getPass);
+    
+    const sql = "SELECT * FROM user";
+    mysqlDB.query(sql,function(err, results) {
+        if(err) console.log(err);
+        else res.send(results);
+        console.log(results);
+    });
+
+    
+});
+
+const mysqlDB = mysql.createConnection({   //express mysql conect
+    host:'kosa2.iptime.org', 
+    user:'rok', 
+    password:'1234', 
+    port:50324, 
+    database:'chat_db' 
 });
 
 // //배포
