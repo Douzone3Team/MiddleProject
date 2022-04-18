ce6fd368afb69b139e37f513d2887fac125ac358
bongjae <bong8230@naver.com>
2022-04-18 10:43:00 +0900

    update

--- argon-dashboard-react-master/client/src/views/Index.js
+++ argon-dashboard-react-master/client/src/views/Index.js
@@ -1,52 +1,66 @@
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 // node.js library that concatenates classes (strings)
-import classnames from "classnames";
-// javascipt plugin for creating charts
-import Chart from "chart.js";
-// react plugin used to create charts
-import { Line, Bar } from "react-chartjs-2";
+
+import socket from "../client_socket"
+
 // reactstrap components
 import {
   Button,
   Card,
   CardHeader,
   CardBody,
-  NavItem,
-  NavLink,
-  Nav,
-  Progress,
+  Input,
   Table,
   Container,
   Row,
   Col,
   CardTitle,
+  Form,
 } from "reactstrap";
 
-// core components
-import {
-  chartOptions,
-  parseOptions,
-  chartExample1,
-  chartExample2,
-} from "variables/charts.js";
-
 import Header from "components/Headers/Header.js";
-import CreateRoom from "components/CreateRoom.js";
+import { Link } from "react-router-dom";
+// import CreateRoom from "components/CreateRoom.js";
 
 
 const Index = (props) => {
-  const [activeNav, setActiveNav] = useState(1);
-  const [chartExample1Data, setChartExample1Data] = useState("data1");
+  const roomRef = useRef();
+  const [roomName, setRoomName] = useState("");
+  const [roomNames, setRoomNames] = useState([]);
+  const userRef = useRef();
+  const [err, setErr] = useState(false);
+  const [errMsg, setErrMsg] = useState('');
 
-  if (window.Chart) {
-    parseOptions(Chart, chartOptions());
+  const CreateRoom = (event) => setRoomName(event.target.value);
+  const onSubmit = (event) => {
+    // const roomName = roomRef.current.value;
+    event.preventDefault();
+    // if (!roomName) {
+    //   setErr(true);
+    //   setErrMsg('Room Name');
+    // } else {
+    //   socket.emit('BE-check-user', { roomId: roomName });
+    // }
+    setRoomNames((currentArray) => [...currentArray, roomName]);
+    setRoomName(" ");
   }
 
-  const toggleNavs = (e, index) => {
-    e.preventDefault();
-    setActiveNav(index);
-    setChartExample1Data("data" + index);
-  };
+  useEffect(() => {
+    socket.on('FE-error-user-exist', ({ error }) => {
+      if (!error) {
+        const roomName = roomRef.current.value;
+
+        props.history.push(`/room/${roomName}`);
+      }
+    });
+  }, [props.history]);
+
+  function clickJoin() {
+    const Rooms = roomRef.current.value;;
+
+  }
+
+
   return (
     <>
       <Header />
@@ -176,79 +190,44 @@ const Index = (props) => {
           </Col>
         </Row> */}
         <Row className="mt-5">
-          <Col className="mb-5 mb-xl-0" xl="8">
+          <Col className="mb-5 mb-xl-0" >
             <Card className="shadow">
               <CardHeader className="border-0">
-                <Row className="align-items-center">
-                  <div className="col">
-                    <h3 className="mb-0">Page visits</h3>
-                  </div>
-                  <div className="col text-right">
-                    {/* <Button
-                      color="primary"
-                      href="#pablo"
-                      onClick={(e) => e.preventDefault()}
-                      size="sm"
-                    >
-                      See all
-                    </Button> */}
-                    <CreateRoom/>
-                  </div>
+                <Row className="align-items-center" >
+                  <Col className="col-sm-5">
+                    <h3 className="mb-0">방 목록</h3>
+                  </Col>
+                  <Col className="" >
+                    {/* roomName Input */}
+                    <Form onSubmit={onSubmit}>
+                      <Input className="text-right" size="" type="text" id="roomName"
+                        ref={roomRef} placeholder="방 이름" onChange={CreateRoom} />
+                      <Button
+                        color="primary"
+                        value={roomName}
+                        size="sm"
+                      >방 생성</Button>
+                    </Form>
+                  </Col>
+
                 </Row>
               </CardHeader>
-              <Table className="align-items-center table-flush" responsive>
+              <Table hover className="align-items-center table-flush" responsive>
                 <thead className="thead-light">
                   <tr>
-                    <th scope="col">Page name</th>
-                    <th scope="col">Visitors</th>
-                    <th scope="col">Unique users</th>
-                    <th scope="col">Bounce rate</th>
+                    <th >방 이름</th>
+                    <th scope="col"> </th>
+                    <th scope="col"> </th>
+                    <th scope="col">참여인원</th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <th scope="row">/argon/</th>
-                    <td>4,569</td>
-                    <td>340</td>
-                    <td>
-                      <i className="fas fa-arrow-up text-success mr-3" /> 46,53%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">/argon/index.html</th>
-                    <td>3,985</td>
-                    <td>319</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-warning mr-3" />{" "}
-                      46,53%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">/argon/charts.html</th>
-                    <td>3,513</td>
-                    <td>294</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-warning mr-3" />{" "}
-                      36,49%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">/argon/tables.html</th>
-                    <td>2,050</td>
-                    <td>147</td>
-                    <td>
-                      <i className="fas fa-arrow-up text-success mr-3" /> 50,87%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">/argon/profile.html</th>
-                    <td>1,795</td>
-                    <td>190</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-danger mr-3" />{" "}
-                      46,53%
-                    </td>
-                  </tr>
+                  {roomNames.map((item, index) => (
+                    <tr key={index}>
+                      <th >{item}</th>
+                      <td><Button color="primary" size="sm"><Link to={`/room/${roomName}`}>참여</Link></Button></td>
+                    </tr>
+                  ))}
                 </tbody>
               </Table>
             </Card>
