9c55a6da3bba5bcb47f38914fbd6860b5f1d244e
chaerin99 <cofls990111@naver.com>
2022-04-20 19:34:21 +0900

    chatting modify2

--- argon-dashboard-react-master/server/server.js
+++ argon-dashboard-react-master/server/server.js
@@ -65,17 +65,19 @@ const mysqlDB = mysql.createConnection({   //express mysql conect
 
 io.on('connection', (socket) => { //소켓이 연결됐을때
     console.log(`New User connected: ${socket.id}`);
-    socket.on('message',({name,message}) => { 
-        console.log('message: '+message+'name: '+name );
-        io.emit('message',({name, message}))
-    })
+
+    // 채팅연결 - git
+    socket.on('BE-send-message', ({msg, sender}) => {
+        io.sockets.emit('FE-receive-message', { msg, sender});
+    });
+
     //연결해제
     socket.on('disconnect', () => {
         socket.disconnect();
-        // console.log('User disconnected!');
+        console.log('User disconnected!');
     });
 
-    //
+    // //
     socket.on('BE-check-user', ({ roomId, userName }) => {
         let error = false;
         console.log(roomId);
@@ -93,66 +95,66 @@ io.on('connection', (socket) => { //소켓이 연결됐을때
 
 
 
-    //Join Room
-    socket.on('BE-join-room', ({ roomId, userName }) => {
-        // Socket Join RoomName
-        socket.join(roomId);
-        socketList[socket.id] = { userName, video: true, audio: true };
+    // //Join Room
+    // socket.on('BE-join-room', ({ roomId, userName }) => {
+    //     // Socket Join RoomName
+    //     socket.join(roomId);
+    //     socketList[socket.id] = { userName, video: true, audio: true };
 
-        // Set User List
-        io.sockets.in(roomId).clients((err, clients) => {
-            try {
-                const users = [];
-                clients.forEach((client) => {
-                    // Add User List
-                    users.push({ userId: client, info: socketList[client] });
-                });
-                socket.broadcast.to(roomId).emit('FE-user-join', users);
-                // io.sockets.in(roomId).emit('FE-user-join', users);
-            } catch (e) {
-                io.sockets.in(roomId).emit('FE-error-user-exist', { err: true });
-            }
-        });
-    });
+    //     // Set User List
+    //     io.sockets.in(roomId).clients((err, clients) => {
+    //         try {
+    //             const users = [];
+    //             clients.forEach((client) => {
+    //                 // Add User List
+    //                 users.push({ userId: client, info: socketList[client] });
+    //             });
+    //             socket.broadcast.to(roomId).emit('FE-user-join', users);
+    //             // io.sockets.in(roomId).emit('FE-user-join', users);
+    //         } catch (e) {
+    //             io.sockets.in(roomId).emit('FE-error-user-exist', { err: true });
+    //         }
+    //     });
+    // });
 
 
-    socket.on('BE-call-user', ({ userToCall, from, signal }) => {
-        io.to(userToCall).emit('FE-receive-call', {
-            signal,
-            from,
-            info: socketList[socket.id],
-        });
-    });
+    // socket.on('BE-call-user', ({ userToCall, from, signal }) => {
+    //     io.to(userToCall).emit('FE-receive-call', {
+    //         signal,
+    //         from,
+    //         info: socketList[socket.id],
+    //     });
+    // });
 
-    socket.on('BE-accept-call', ({ signal, to }) => {
-        io.to(to).emit('FE-call-accepted', {
-            signal,
-            answerId: socket.id,
-        });
-    });
+    // socket.on('BE-accept-call', ({ signal, to }) => {
+    //     io.to(to).emit('FE-call-accepted', {
+    //         signal,
+    //         answerId: socket.id,
+    //     });
+    // });
 
-    socket.on('BE-send-message', ({ roomId, msg, sender }) => {
-        io.sockets.in(roomId).emit('FE-receive-message', { msg, sender });
-    });
+    // socket.on('BE-send-message', ({ roomId, msg, sender }) => {
+    //     io.sockets.in(roomId).emit('FE-receive-message', { msg, sender });
+    // });
 
-    socket.on('BE-leave-room', ({ roomId, leaver }) => {
-        delete socketList[socket.id];
-        socket.broadcast
-            .to(roomId)
-            .emit('FE-user-leave', { userId: socket.id, userName: [socket.id] });
-        io.sockets.sockets[socket.id].leave(roomId);
-    });
+    // socket.on('BE-leave-room', ({ roomId, leaver }) => {
+    //     delete socketList[socket.id];
+    //     socket.broadcast
+    //         .to(roomId)
+    //         .emit('FE-user-leave', { userId: socket.id, userName: [socket.id] });
+    //     io.sockets.sockets[socket.id].leave(roomId);
+    // });
 
-    socket.on('BE-toggle-camera-audio', ({ roomId, switchTarget }) => {
-        if (switchTarget === 'video') {
-            socketList[socket.id].video = !socketList[socket.id].video;
-        } else {
-            socketList[socket.id].audio = !socketList[socket.id].audio;
-        }
-        socket.broadcast
-            .to(roomId)
-            .emit('FE-toggle-camera', { userId: socket.id, switchTarget });
-    });
+    // socket.on('BE-toggle-camera-audio', ({ roomId, switchTarget }) => {
+    //     if (switchTarget === 'video') {
+    //         socketList[socket.id].video = !socketList[socket.id].video;
+    //     } else {
+    //         socketList[socket.id].audio = !socketList[socket.id].audio;
+    //     }
+    //     socket.broadcast
+    //         .to(roomId)
+    //         .emit('FE-toggle-camera', { userId: socket.id, switchTarget });
+    // });
 });
 
 
