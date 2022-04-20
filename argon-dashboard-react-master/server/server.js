cef7c4eaddd01b5624a2b554bda8da383a0652f9
chaerin99 <cofls990111@naver.com>
2022-04-20 20:29:23 +0900

    room수정

--- argon-dashboard-react-master/server/server.js
+++ argon-dashboard-react-master/server/server.js
@@ -124,10 +124,6 @@ io.on('connection', (socket) => { //소켓이 연결됐을때
         // });
     });
 
-
-
-
-
     //Join Room
     // socket.on('BE-join-room', ({ roomId, userName }) => {
     //     // Socket Join RoomName
