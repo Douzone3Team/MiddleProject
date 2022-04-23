c668c29facee9f64cfb163a643ae2603ccb7c882
bongjae <bong8230@naver.com>
2022-04-23 16:22:58 +0900

    error modify

--- argon-dashboard-react-master/client/src/views/examples/Profile.js
+++ argon-dashboard-react-master/client/src/views/examples/Profile.js
@@ -3,7 +3,7 @@
 // 접속한 사용자의 기존 정보를 수정할 페이지
 
 
-import React, { useEffect } from 'react' 
+import React, { useEffect } from 'react'
 
 import { Card, CardHeader, CardBody, Container, Row, Col, Form, FormGroup, Input, Button } from "reactstrap";
 import { BsPersonCircle, BsPencilSquare } from 'react-icons/bs'
@@ -15,12 +15,12 @@ import Header from "components/Headers/Header.js";
 import axios from 'axios';
 import Cookies from 'universal-cookie';
 
-function Profile(props) {   
+function Profile(props) {
   const cookie = new Cookies();
   const myName = cookie.get("myname");
   const myId = cookie.get("myId");
-  
-  const loginCheck = async() => {
+
+  const loginCheck = async () => {
     if (!cookie.get("user")) {
       //쿠키가 없을때 로그인 페이지로 강제로 이동시킴
       alert("로그인을 해주세요");
@@ -44,25 +44,25 @@ function Profile(props) {
         .catch((ex) => {
           console.log(ex);
         });
-      }
-    };
+    }
+  };
   useEffect(() => {
     loginCheck();
   })
-  
+
   return (
-    <> 
+    <>
       <div className="header pb-8 pt-5 pt-lg-8 d-flex align-items-center"
         style={{ minHeight: "100px", backgroundSize: "cover", backgroundPosition: "center top" }}
-      > 
-        <span className="mask bg-gradient-default opacity-8" /> 
+      >
+        <span className="mask bg-gradient-default opacity-8" />
         <Container>
           <Col lg="12">
             <div className="card-profile-image">
               <a href="#pablo" onClick={(e) => e.preventDefault()}>
                 {/* 이미지가 없을 경우 보여줄 아이콘 -> 반응형으로 작업(가운데로) 필요 */}
                 {/* 추가 작업 : 이미지 onClick 시 프로필 사진 변경 */}
-                <img alt="..." className="rounded-circle" rc={ require("../../assets/img/theme/team-4-800x800.jpg").default } />
+                <img alt="..." className="rounded-circle" rc={require("../../assets/img/theme/team-4-800x800.jpg").default} />
               </a>
             </div>
           </Col>
@@ -71,44 +71,34 @@ function Profile(props) {
               <div className="search_box" style={{ textAlign: 'center' }}>
                 <BsPersonCircle size={120} color={'white'} />
                 {/* 로그인한 유저명 보여주기 */}
-                <h1 className="display-2 text-white"> { myName } </h1>
-                <p className="text-white mt-0 mb-5"> ID : { myId } </p> 
+                <h1 className="display-2 text-white"> {myName} </h1>
+                <p className="text-white mt-0 mb-5"> ID : {myId} </p>
               </div>
             </Col>
-            <Row>
-              <Col md="12">
-                <div className="search_box" style={{ textAlign: 'center' }}>
-                  <BsPersonCircle size={120} color={'white'} />
-                  {/* 로그인한 유저명 보여주기 */}
-                  <h1 className="display-2 text-white">Hello, Douzone !</h1>
-                  {/* 마지막 접속일자 띄워주기 (현재는 오늘 날짜 가져오도록 임의로 넣어놓음) */}
-                  <p className="text-white mt-0 mb-5"> 마지막 접속일자 : {today.getFullYear() + "/" + today.getDay()} </p>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </div>
+          </Row>
+        </Container>
+      </div>
 
 
       {/* 로그인한 계정의 정보 변경 카드 */}
       {/* <Container className="mt--7" fluid>   */}
-      <Fade bottom duration={ 1500 }>
-        <Container className="mt--7" style={{width: '60%'}}>  
+      <Fade bottom duration={1500}>
+        <Container className="mt--7" style={{ width: '60%' }}>
           <Col className="order-xl-2 mb-5 mb-xl-0" xl="12">
             <Card className="bg-secondary shadow">
               <Form>
                 <CardHeader className="bg-white border-0">
                   <Row className="align-items-center">
                     <Col xs="12">
-                      <h3 className="mb-0">My account &nbsp; <BsPencilSquare/>
-                        <Button className="float-right" color="primary"  
+                      <h3 className="mb-0">My account &nbsp; <BsPencilSquare />
+                        <Button className="float-right" color="primary"
                           onClick={(e) => e.preventDefault()} size="sm"
-                        >저장</Button>  
+                        >저장</Button>
                       </h3>
-                    </Col> 
+                    </Col>
                   </Row>
                 </CardHeader>
-              <CardBody>
+                <CardBody>
                   <h6 className="heading-small text-muted mb-4"> User information </h6>
                   <div className="pl-lg-4">
                     <Row>
@@ -117,7 +107,7 @@ function Profile(props) {
                           <label className="form-control-label"> 유저명 </label>
                           <Input className="form-control-alternative" id="input-username" type="text" />
                         </FormGroup>
-                      </Col> 
+                      </Col>
                       <Col lg="6">
                         <FormGroup>
                           <label className="form-control-label"> 암호 변경 </label>
@@ -131,9 +121,9 @@ function Profile(props) {
                           <Input className="form-control-alternative" id="input-pwd2" type="password2" />
                         </FormGroup>
                       </Col>
-                    </Row> 
+                    </Row>
                   </div>
-                  <hr className="my-4" /> 
+                  <hr className="my-4" />
                   {/* Description */}
                   <h6 className="heading-small text-muted mb-4">About me</h6>
                   <div className="pl-lg-4">
@@ -142,17 +132,17 @@ function Profile(props) {
                       <Input className="form-control-alternative" placeholder="안녕하세요" rows="3" type="textarea" />
                     </FormGroup>
                   </div>
-              </CardBody>
+                </CardBody>
 
-                </Form>
-              </Card>
-            </Col>
-          </Container>
-        </Fade>
-        <Container fluid>
-          <AdminFooter />
+              </Form>
+            </Card>
+          </Col>
         </Container>
-      </div>
+      </Fade>
+      <Container fluid>
+        <AdminFooter />
+      </Container>
+
     </>
   );
 }
